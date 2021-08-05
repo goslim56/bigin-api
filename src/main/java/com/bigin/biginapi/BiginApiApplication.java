ca5534480522caa05b0d5e2ef418d5e454a01519
ghkim04 <ghkim04@cafe24corp.com>
2021-08-05 22:24:41 +0900

    project creation

---
+++ src/main/java/com/bigin/biginapi/BiginApiApplication.java
@@ -0,0 +1,13 @@
+package com.bigin.biginapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiginApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigin/biginapi/BiginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigin.biginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
